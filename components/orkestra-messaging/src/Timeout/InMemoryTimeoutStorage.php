a6fd6b304ad21e4d904b9f6b9756b3c660407191
jwillp <jwilliam.perreault@morebec.com>
2021-07-01 18:45:41 -0400

    [Messaging] Add InMemoryTimeoutStorage implementation

---
+++ components/orkestra-messaging/src/Timeout/InMemoryTimeoutStorage.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Morebec\Orkestra\Messaging\Timeout;
+
+use Morebec\Orkestra\DateTime\DateTime;
+
+class InMemoryTimeoutStorage implements TimeoutStorageInterface
+{
+    /** @var TimeoutWrapper[] */
+    private $wrappers;
+
+    public function __construct()
+    {
+        $this->wrappers = [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function add(TimeoutWrapper $wrapper): void
+    {
+        $this->wrappers[$wrapper->getId()] = $wrapper;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findByEndsAtBefore(DateTime $dateTime): array
+    {
+        return array_filter($this->wrappers, static function (TimeoutWrapper $wrapper) use ($dateTime) {
+            $endsAt = $wrapper->getTimeout()->getEndsAt();
+
+            return $endsAt->isBefore($dateTime) || $endsAt->isSameDay($dateTime);
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findByEndsAtBetween(DateTime $from, DateTime $to): array
+    {
+        return array_filter($this->wrappers, static function (TimeoutWrapper $wrapper) use ($from, $to) {
+            return $wrapper->getTimeout()->getEndsAt()->isBetween($from, $to);
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function remove(string $timeoutId): void
+    {
+        unset($this->wrappers[$timeoutId]);
+    }
+}
---
+++ components/orkestra-messaging/src/Timeout/InMemoryTimeoutStorageTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Morebec\Orkestra\Messaging\Timeout;
+
+use Morebec\Orkestra\DateTime\DateTime;
+use Morebec\Orkestra\Messaging\MessageHeaders;
+use PHPUnit\Framework\TestCase;
+
+class InMemoryTimeoutStorageTest extends TestCase
+{
+    public function testFindByEndsAtBetween(): void
+    {
+        $storage = new InMemoryTimeoutStorage();
+        $storage->add(TimeoutWrapper::wrap(new class() implements TimeoutInterface {
+            public static function getTypeName(): string
+            {
+                return 'test_timeout';
+            }
+
+            public function getId(): string
+            {
+                return uniqid('timeout_', false);
+            }
+
+            public function getEndsAt(): DateTime
+            {
+                return new DateTime('2020-01-01');
+            }
+        }, new MessageHeaders()));
+
+        $results = $storage->findByEndsAtBetween(new DateTime('2020-01-01'), new DateTime('2020-01-05'));
+        self::assertCount(1, $results);
+    }
+
+    public function testRemove(): void
+    {
+        $storage = new InMemoryTimeoutStorage();
+        $id = uniqid('timeout_', false);
+        $storage->add(TimeoutWrapper::wrap(new class($id) implements TimeoutInterface {
+            private string $id;
+
+            public function __construct(string $id)
+            {
+                $this->id = $id;
+            }
+
+            public static function getTypeName(): string
+            {
+                return 'test_timeout';
+            }
+
+            public function getId(): string
+            {
+                return $this->id;
+            }
+
+            public function getEndsAt(): DateTime
+            {
+                return new DateTime('2020-01-01');
+            }
+        }, new MessageHeaders()));
+
+        $storage->remove($id);
+
+        $results = $storage->findByEndsAtBetween(new DateTime('2020-01-01'), new DateTime('2020-05-05'));
+
+        self::assertCount(0, $results);
+    }
+
+    public function testFindByEndsAtBefore(): void
+    {
+        $storage = new InMemoryTimeoutStorage();
+        $id = uniqid('timeout_', false);
+        $storage->add(TimeoutWrapper::wrap(new class($id) implements TimeoutInterface {
+            private string $id;
+
+            public function __construct(string $id)
+            {
+                $this->id = $id;
+            }
+
+            public static function getTypeName(): string
+            {
+                return 'test_timeout';
+            }
+
+            public function getId(): string
+            {
+                return $this->id;
+            }
+
+            public function getEndsAt(): DateTime
+            {
+                return new DateTime('2020-01-01');
+            }
+        }, new MessageHeaders()));
+
+        $results = $storage->findByEndsAtBefore(new DateTime('2020-01-02'));
+        self::assertCount(1, $results);
+    }
+
+    public function testAdd(): void
+    {
+        $storage = new InMemoryTimeoutStorage();
+        $storage->add(TimeoutWrapper::wrap(new class() implements TimeoutInterface {
+            public static function getTypeName(): string
+            {
+                return 'test_timeout';
+            }
+
+            public function getId(): string
+            {
+                return uniqid('timeout_', false);
+            }
+
+            public function getEndsAt(): DateTime
+            {
+                return new DateTime('2020-01-01');
+            }
+        }, new MessageHeaders()));
+
+        $results = $storage->findByEndsAtBetween(new DateTime('2020-01-01'), new DateTime('2020-01-05'));
+        self::assertCount(1, $results);
+    }
+}
