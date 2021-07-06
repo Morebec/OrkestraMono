028eef538f232585b535a59aaa5eb3396a194977
jwillp <jwilliam.perreault@morebec.com>
2021-07-06 14:21:09 -0400

    [Symfony Bundle] Fix TimeoutConfigurationProcessor overriding service definitions

--- components/orkestra-symfony-bundle/DependencyInjection/Configuration/TimeoutProcessingConfigurationProcessor.php
+++ components/orkestra-symfony-bundle/DependencyInjection/Configuration/TimeoutProcessingConfigurationProcessor.php
@@ -4,6 +4,7 @@ namespace Morebec\Orkestra\SymfonyBundle\DependencyInjection\Configuration;
 
 use Morebec\Orkestra\Messaging\Timeout\TimeoutManagerInterface;
 use Morebec\Orkestra\Messaging\Timeout\TimeoutStorageInterface;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 
 class TimeoutProcessingConfigurationProcessor
 {
@@ -11,7 +12,16 @@ class TimeoutProcessingConfigurationProcessor
         OrkestraConfiguration $orkestraConfiguration,
         TimeoutProcessingConfiguration $configuration
     ): void {
-        $orkestraConfiguration->service(TimeoutManagerInterface::class, $configuration->managerImplementationClassName);
-        $orkestraConfiguration->service(TimeoutStorageInterface::class, $configuration->storageImplementationClassName);
+        try {
+            $orkestraConfiguration->container()->services()->get(TimeoutManagerInterface::class);
+        } catch (ServiceNotFoundException $exception) {
+            $orkestraConfiguration->service(TimeoutManagerInterface::class, $configuration->managerImplementationClassName);
+        }
+
+        try {
+            $orkestraConfiguration->container()->services()->get(TimeoutStorageInterface::class);
+        } catch (ServiceNotFoundException $exception) {
+            $orkestraConfiguration->service(TimeoutStorageInterface::class, $configuration->storageImplementationClassName);
+        }
     }
 }
