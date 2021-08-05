c62dea928e6dbfc6f6a8dff644504600bd7c9edc
jwillp <jwilliam.perreault@morebec.com>
2021-08-05 04:08:00 -0400

    [SymfonyBundle] Add Web related classes for use with spectool

---
+++ components/orkestra-symfony-bundle/Web/HttpObjectNormalizer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Morebec\Orkestra\SymfonyBundle\Web;
+
+use Morebec\Orkestra\Normalization\ObjectNormalizer;
+
+class HttpObjectNormalizer extends ObjectNormalizer implements HttpObjectNormalizerInterface
+{
+}
