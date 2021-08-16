02002b8d5f82113861b06d223d9aeff1abefd663
jwillp <jwilliam.perreault@morebec.com>
2021-08-16 15:29:05 -0400

    [Framework] Make framework a library of helper classes

--- components/orkestra-symfony-bundle/Web/HttpObjectNormalizer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Morebec\Orkestra\SymfonyBundle\Web;
-
-use Morebec\Orkestra\Normalization\ObjectNormalizer;
-
-class HttpObjectNormalizer extends ObjectNormalizer implements HttpObjectNormalizerInterface
-{
-}
